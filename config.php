10db0899509af969969b20108084239e468bf9c0
Anam <66148129+anam0077@users.noreply.github.com>
2021-11-13 15:33:11 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define ('BASEURL', 'https://www.englishjet.net');
+
+//db
+
+define ('DB_HOST', 'localhost');
+define ('DB_USER', 'user_name');
+define ('DB_PASS', '');
+define ('DB_NAME', '');
