025b85f5fb0e010ea83e38d92102e367308ddbd9
Anam <66148129+anam0077@users.noreply.github.com>
2021-11-13 04:26:49 -0600

    Create index.php

---
+++ app/views/module/index.php
@@ -0,0 +1,18 @@
+
+			<div id="mainpage" >
+		     <img src="assets/images/home.jpg" class="w-100 rounded">
+			</div>
+		
+<article class="card">
+       		<h2 >Kenapa harus bahasa inggris</h2>
+       			<p>Why should we speak foreign language?especially english.</p>
+       				<ul>
+       					<li>Commonly spoke in the world.</li>
+       					<li>It can help you get better job.</li>
+       					<li>It makes you easy to travel.</li>
+       					<li>It is the language of Media.</li>
+					   </ul>
+					   <h3>Comment:</h3>
+				<div id="contoh1" class="card comment p"></div>
+			
+</article
