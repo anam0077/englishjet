0078d7f7ffc8533e771f272dd8da1f2f9f72e0b1
Anam <66148129+anam0077@users.noreply.github.com>
2021-11-13 02:57:48 -0600

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+define ('BASEURL', 'https://www.englishjet.net'); 
+//db try to use your own db
+define ('DB_HOST', 'localhost');
+define ('DB_USER', '');
+define ('DB_PASS', '');
+define ('DB_NAME', '');
