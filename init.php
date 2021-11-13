1a05af174abd25e2ae700c74a1df455df9b12016
Anam <66148129+anam0077@users.noreply.github.com>
2021-11-13 15:29:51 +0700

    Add files via upload

---
+++ init.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'App.php';
+require_once 'Controller.php';
+require_once 'Database.php';
+require_once 'Flasher.php';
+require_once 'FileConcern.php';
+require_once 'Informer.php';
+require_once 'config.php';
