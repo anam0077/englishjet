f057c865f87ffe97f452ce54a019706fe0a470cf
Anam <66148129+anam0077@users.noreply.github.com>
2021-11-13 02:44:19 -0600

    Delete init.php

--- init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'App.php';
-require_once 'Controller.php';
-require_once 'Database.php';
-require_once 'Flasher.php';
-require_once 'FileConcern.php';
-require_once 'Informer.php';
-require_once 'config.php';
